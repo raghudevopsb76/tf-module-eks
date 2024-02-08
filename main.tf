41a2e9c4c4511c498e40dad43c69d88ee05ba840
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-08 06:57:31 +0530

    RoboShop TF Code

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_iam_role" "main_nodegroup" {
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "ec2.amazonaws.com"
+        Service = ["ec2.amazonaws.com"]
       }
     }]
     Version = "2012-10-17"
