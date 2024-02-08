b1ec01da3c8e40ddd6d11ba7a4777ff2e2986f17
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-08 06:46:37 +0530

    RoboShop TF Code

--- main.tf
+++ main.tf
@@ -37,3 +37,47 @@ resource "aws_eks_cluster" "main" {
 
 }
 
+resource "aws_iam_role" "main" {
+  name = "${var.env}-eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.main.name
+}
+
+resource "aws_iam_role_policy_attachment" "main-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.main.name
+}
+
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.env}-eks-ng"
+  node_role_arn   = aws_iam_role.main.arn
+  subnet_ids      = var.subnets
+  instance_types  = var.instance_types
+
+  scaling_config {
+    desired_size = var.node_count
+    max_size     = var.node_count
+    min_size     = var.node_count
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,5 @@ variable "tags" {}
 variable "kms" {}
 variable "subnets" {}
 variable "vpc_id" {}
+variable "node_count" {}
+variable "instance_types" {}
